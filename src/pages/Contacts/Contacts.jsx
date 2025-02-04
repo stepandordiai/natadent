8b831ca96756a38bbf5d1558cfdc4df68c59e302
Stepan Dordiai <stepandordiai@gmail.com>
2025-02-04 13:22:38 +0100

    v2.14.0 Add custom select for contact

--- src/pages/Contacts/Contacts.jsx
+++ src/pages/Contacts/Contacts.jsx
@@ -29,6 +29,59 @@ const Contacts = () => {
                     }
                 });
             });
+
+        // Listener for multiple custom selectors
+        document
+            .querySelectorAll(".contacts-form__custom-select")
+            .forEach((select) => {
+                const dropdownBtn = select.querySelector(
+                    ".contacts-form__dropdown-btn"
+                );
+                const dropdownList = select.querySelector(
+                    ".contacts-form__dropdown-list"
+                );
+                const dropdownOptions = dropdownList.querySelectorAll(
+                    ".contacts-form__dropdown-option"
+                );
+                const dropdownInput = document.querySelector(
+                    ".contacts-form__dropdown-input"
+                );
+
+                dropdownBtn.addEventListener("click", (e) => {
+                    // Prevent from submitting a form
+                    e.preventDefault();
+                    dropdownList.classList.toggle(
+                        "contacts-form__dropdown-list--visible"
+                    );
+                    dropdownBtn.classList.add(
+                        "contacts-form__dropdown-btn--active"
+                    );
+                });
+
+                dropdownOptions.forEach((option) => {
+                    option.addEventListener("click", (e) => {
+                        // TODO:
+                        e.stopPropagation();
+                        dropdownBtn.textContent = option.textContent;
+                        dropdownBtn.focus();
+                        dropdownInput.value = option.dataset.value;
+                        dropdownList.classList.remove(
+                            "contacts-form__dropdown-list--visible"
+                        );
+                    });
+                });
+
+                document.addEventListener("click", (e) => {
+                    if (e.target !== dropdownBtn) {
+                        dropdownBtn.classList.remove(
+                            "contacts-form__dropdown-btn--active"
+                        );
+                        dropdownList.classList.remove(
+                            "contacts-form__dropdown-list--visible"
+                        );
+                    }
+                });
+            });
     }, []);
 
     return (
@@ -83,7 +136,6 @@ const Contacts = () => {
                     </a>
                 </div>
             </div>
-
             <CustomDivider />
             <h3 className="contacts__appointment-title">Make an appointment</h3>
             <form
@@ -100,6 +152,7 @@ const Contacts = () => {
                         <input
                             className="contacts-form__input  js-contacts-form__input"
                             type="text"
+                            name="First name"
                         />
                     </div>
                     <div className="contacts-form__input-wrapper">
@@ -109,6 +162,7 @@ const Contacts = () => {
                         <input
                             className="contacts-form__input  js-contacts-form__input"
                             type="text"
+                            name="Last name"
                         />
                     </div>
                 </div>
@@ -118,6 +172,7 @@ const Contacts = () => {
                         <input
                             className="contacts-form__input js-contacts-form__input"
                             type="text"
+                            name="Email"
                         />
                     </div>
                     <div className="contacts-form__input-wrapper">
@@ -127,25 +182,85 @@ const Contacts = () => {
                         <input
                             className="contacts-form__input  js-contacts-form__input"
                             type="text"
+                            name="Phone number"
                         />
                     </div>
                 </div>
-
-                <select className="contacts-form__select" name="" id="">
-                    <option value="">Choose service</option>
-                    <option value="">1</option>
-                    <option value="">2</option>
-                    <option value="">3</option>
-                    <option value="">4</option>
-                </select>
-                <input type="date" defaultValue="2024-12-12" />
-                <input type="time" defaultValue="12:00" />
+                {/* Custom select */}
+                <div className="contacts-form__custom-select">
+                    <button className="contacts-form__dropdown-btn">
+                        Choose department
+                    </button>
+                    <ul className="contacts-form__dropdown-list">
+                        <li
+                            className="contacts-form__dropdown-option"
+                            data-value="Not selected"
+                        >
+                            Select department
+                        </li>
+                        <li
+                            className="contacts-form__dropdown-option"
+                            data-value="Oral Hygienist"
+                        >
+                            Oral Hygienist
+                        </li>
+                        <li
+                            className="contacts-form__dropdown-option"
+                            data-value="Teeth Cleaning"
+                        >
+                            Teeth Cleaning
+                        </li>
+                        <li
+                            className="contacts-form__dropdown-option"
+                            data-value="Care Advice"
+                        >
+                            Care Advice
+                        </li>
+                        <li
+                            className="contacts-form__dropdown-option"
+                            data-value="Dental Care"
+                        >
+                            Dental Care
+                        </li>
+                        <li
+                            className="contacts-form__dropdown-option"
+                            data-value="Tooth Protection"
+                        >
+                            Tooth Protection
+                        </li>
+                        <li
+                            className="contacts-form__dropdown-option"
+                            data-value="Whitening Teeth"
+                        >
+                            Whitening Teeth
+                        </li>
+                        <li
+                            className="contacts-form__dropdown-option"
+                            data-value="Dental Implants"
+                        >
+                            Dental Implants
+                        </li>
+                        <li
+                            className="contacts-form__dropdown-option"
+                            data-value="Oral Surgery"
+                        >
+                            Oral Surgery
+                        </li>
+                    </ul>
+                    <input
+                        className="contacts-form__dropdown-input"
+                        type="text"
+                        name="Department"
+                        defaultValue=""
+                    />
+                </div>
+                <input type="date" defaultValue="2024-12-12" name="Date" />
+                <input type="time" defaultValue="12:00" name="Time" />
                 <button className="contacts-form__btn" type="submit">
                     Book an appointment
                 </button>
             </form>
             <CustomDivider />
-
             <iframe
                 className="contacts__google-map"
                 src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d5126.647298667424!2d15.214303!3d50.024031!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x470c153bee9daf87%3A0xc91ccf8d204d4cb!2sPod%20Hroby%20271%2F271%2C%20280%2002%20Kol%C3%ADn%20IV!5e0!3m2!1scs!2scz!4v1738164855741!5m2!1scs!2scz"
