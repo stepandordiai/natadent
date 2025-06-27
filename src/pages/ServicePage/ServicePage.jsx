06595b1214f72d70b8a35ac79a51eb47031a8a11
Stepan Dordiai <stepandordiai@gmail.com>
2025-06-27 16:32:41 +0200

    Update ServicePage

--- src/pages/ServicePage/ServicePage.jsx
+++ src/pages/ServicePage/ServicePage.jsx
@@ -1,7 +1,9 @@
 import { Helmet } from "react-helmet";
 import { useTranslation } from "react-i18next";
 import PageTitle from "../../components/PageTitle/PageTitle";
-import { useParams } from "react-router-dom";
+import { useParams, NavLink } from "react-router-dom";
+import prevIcon from "/icons/back.png";
+import nextIcon from "/icons/next.png";
 import "./ServicePage.scss";
 
 const ServicePage = ({ servicesData }) => {
@@ -10,6 +12,14 @@ const ServicePage = ({ servicesData }) => {
 	const { id } = useParams();
 
 	const service = servicesData.find((service) => service.id === id);
+
+	const currentIndex = servicesData.findIndex((service) => service.id === id);
+
+	const nextService = servicesData[(currentIndex + 1) % servicesData.length];
+	const prevService =
+		currentIndex - 1 !== -1
+			? servicesData[currentIndex - 1]
+			: servicesData[servicesData.length - 1];
 
 	return (
 		<>
@@ -21,6 +31,28 @@ const ServicePage = ({ servicesData }) => {
 			<main className="service-page">
 				<PageTitle title={t(service.name)} previousPath={t("services_title")} />
 				<p className="coming-soon">{t("coming_soon")}</p>
+				<div className="service-page__links">
+					<NavLink
+						className={"service-page__link"}
+						to={`/service-page/${prevService.id}`}
+					>
+						<span>
+							<img width={20} height={20} src={prevIcon} alt="" />
+							<span>Previous</span>
+						</span>
+						<span>{t(prevService.name)}</span>
+					</NavLink>
+					<NavLink
+						className={"service-page__link"}
+						to={`/service-page/${nextService.id}`}
+					>
+						<span>
+							<span>Next</span>
+							<img width={20} height={20} src={nextIcon} alt="" />
+						</span>
+						<span>{t(nextService.name)}</span>
+					</NavLink>
+				</div>
 			</main>
 		</>
 	);
